0838490ae85898a16a4c5a779d73c6eb9e0f130a
Chaitali <chaitali@Amuls-MBP-2.bbrouter>
2025-10-08 13:18:01 +0530

    test02 branch

--- src/Clock.js
+++ src/Clock.js
@@ -1,14 +1,14 @@
-import { useEffect, useState } from 'react';
-import './Clock.css';
+import { useEffect, useState } from "react";
+import "./Clock.css";
 
 function Clock() {
   const [time, setTime] = useState(new Date());
 
-  useEffect(()=>{
-    setInterval(()=>{
-      setTime(new Date())
-    },1000)
-  })
+  useEffect(() => {
+    setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+  });
   return (
     <div className="clock">
       <div className="numbers">
@@ -16,13 +16,23 @@ function Clock() {
         <div className="number three">3</div>
         <div className="number six">6</div>
         <div className="number nine">9</div>
-      </div>      
-      <div className='hour_hand'
-      style={{transform: `rotate(${(time.getHours() + time.getMinutes()/60) * 30}deg)`}}></div>
-      <div className='minute_hand'
-      style={{transform: `rotate(${time.getMinutes() * 6}deg)`}}></div>
-      <div className='second_hand'
-      style={{transform: `rotate(${time.getSeconds() * 6}deg)`}}></div>
+      </div>
+      <div
+        className="hour_hand"
+        style={{
+          transform: `rotate(${
+            (time.getHours() + time.getMinutes() / 60) * 30
+          }deg)`,
+        }}
+      ></div>
+      <div
+        className="minute_hand"
+        style={{ transform: `rotate(${time.getMinutes() * 6}deg)` }}
+      ></div>
+      <div
+        className="second_hand"
+        style={{ transform: `rotate(${time.getSeconds() * 6}deg)` }}
+      ></div>
     </div>
   );
 }
